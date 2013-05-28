f3f172f0adf97301530e4bc897fe6ead3564f72c
siddharth-init <sahani.bunty9@gmail.com>
2013-05-28 11:55:32 +0530

    resource center admin interface and frontend added with structuring

--- application/views/fe/resource_center_list.php
+++ application/views/fe/resource_center_list.php
@@ -19,24 +19,49 @@
 
 <div id="recruitment">
       <ul>
+	  <li><h3><u>Annual Report</u></h3></li>
 	  <?php foreach ($resource_center_list as $row):
 	  		if($row->type=='Annual Report'):
 			?>
-			<li><h3><u>Annual Report</u></h3></li>
-			<?php endif;
-			if($row->type=='News Letter'): ?>
-			<li><h3><u>News Letter</u></h3></li>
-			<?php endif;
-			if($row->type=='Brochure'):?>
-			<li><h3><u>Brochure</u></h3></li>
-			<?php endif; ?>
 			<li><strong><?php echo $row->title;?></strong></li>
 			<li><a href="<?php echo site_url('assets/uploads/files/')."/".$row->filename;?>" title="description_1"><?php echo $row->filename;?>  </a></li>
 			<li> <strong><?php echo $row->date;?></strong> </li>
-	<?php endforeach;?>
+	 		<?php endif; ?>
+	 <?php endforeach;?>
     
       </ul>
     </div>
+
+<div id="recruitment">
+      <ul>
+			<li><h3><u>News Letter</u></h3></li>
+	  <?php foreach ($resource_center_list as $row1):
+	  		if($row1->type=='News Letter'):
+			?>
+			<li><strong><?php echo $row1->title;?></strong></li>
+			<li><a href="<?php echo site_url('assets/uploads/files/')."/".$row1->filename;?>" title="description_1"><?php echo $row1->filename;?>  </a></li>
+			<li> <strong><?php echo $row1->date;?></strong> </li>
+			<?php endif; ?>
+	 <?php endforeach;?>
+    
+      </ul>
+    </div>
+
+<div id="recruitment">
+      <ul>
+	  <li><h3><u>Brochure</u></h3></li>
+	  <?php foreach ($resource_center_list as $row2):
+	  		if($row2->type=='Brochure'):
+			?>
+			<li><strong><?php echo $row2->title;?></strong></li>
+			<li><a href="<?php echo site_url('assets/uploads/files/')."/".$row2->filename;?>" title="description_1"><?php echo $row2->filename;?>  </a></li>
+			<li> <strong><?php echo $row2->date;?></strong> </li>
+			<?php endif; ?>
+	 <?php endforeach;?>
+    
+      </ul>
+    </div>
+
 
 <div class="clear"></div>
 
